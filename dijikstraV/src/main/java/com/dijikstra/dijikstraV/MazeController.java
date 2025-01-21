56272fe230cafaf0ce0e1ccb6c52cdd21d955402
HERAK Mohammed <mohammedherak37@gmail.Com>
2025-01-21 16:51:16 +0100

    Add CORS support for MazeController and organize imports

--- dijikstraV/src/main/java/com/dijikstra/dijikstraV/MazeController.java
+++ dijikstraV/src/main/java/com/dijikstra/dijikstraV/MazeController.java
@@ -1,14 +1,23 @@
 package com.dijikstra.dijikstraV;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
-
 @RestController
 @RequestMapping("/api/maze")
+@CrossOrigin(origins = "https://dijikstravisualisation.netlify.app/")
 public class MazeController {
 
     @PostMapping("/dijkstra")
